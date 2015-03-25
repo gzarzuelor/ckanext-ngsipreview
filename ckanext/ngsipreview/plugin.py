d12c20ddeda6411c5e94b88683697870666f1a7a
gzarrub <g.zarrub@gmail.com>
2015-03-25 09:49:20 +0100

    Now system checks if user is logged in only if the resource needs oauth token

--- ckanext/ngsipreview/plugin.py
+++ ckanext/ngsipreview/plugin.py
@@ -81,7 +81,7 @@ class NGSIPreview(p.SingletonPlugin):
         pattern = "/dataset/"+data_dict['package']['name']+"/resource/"
         if format_lower in self.NGSI_FORMATS:
             if resource['on_same_domain'] or self.proxy_is_enabled:
-                if self.check_query(resource) and request.path.find(pattern) != -1 and not toolkit.c.user:
+                if self.check_query(resource) and request.path.find(pattern) != -1 and oauth_req == 'true' and not toolkit.c.user:
                     details = "In order to see this resource properly, you need to be logged in"
                     h.flash_error(details, allow_html=False)
                     return {'can_preview': False, 'fixable': details, 'quality': 2}
